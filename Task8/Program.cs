bfc9aaff68c2a4fb9a6ddb343ca8c74fe6cc850c
Maria <mzobkova@bk.ru>
2022-12-16 16:42:54 +0300

    task8 added

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(ReadLine());
+int b = 1;
+
+if (a >= 1)
+{
+    while (a >= b)
+    {
+        if ((b % 2)==0)
+        {
+            Write($"{b}, ");
+            b = b + 1;
+        }
+        else
+        {
+            b = b + 1;
+        }
+    }
+}
+else 
+{
+    Write("Вы ввели недопустимое значение.");
+}
