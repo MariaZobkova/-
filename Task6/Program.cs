b31b52ed21082fa079e61aec53c93875a1bdbcaa
Maria <mzobkova@bk.ru>
2022-12-16 15:36:22 +0300

    task6 added

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(ReadLine());
+
+if ((a % 2)==0)
+{
+    WriteLine($"Число {a} четное");
+}
+else
+{
+    WriteLine($"Число {a} нечетное");
+}
