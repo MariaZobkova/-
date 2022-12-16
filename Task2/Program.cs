24e8e69c5b35d2744a9a544781d8ba90c9654e06
Maria <mzobkova@bk.ru>
2022-12-16 14:49:36 +0300

    task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+
+int a = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите второе число: ");
+
+int b = Convert.ToInt32(ReadLine());
+
+if (a > b)
+{
+    WriteLine($"Число {a} больше числа {b}");
+}
+else
+{
+    WriteLine($"Число {b} больше числа {a}");
+}
