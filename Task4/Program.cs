d9fc6f6353b863c4496a4db4b2997df7706d055e
Maria <mzobkova@bk.ru>
2022-12-16 15:11:17 +0300

    task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+
+int a = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите второе число: ");
+
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите третье число: ");
+
+int c = Convert.ToInt32(ReadLine());
+
+if (a > b && a > c)
+{
+    WriteLine($"Max = {a}");
+}
+else if (b > a && b > c)
+{
+    WriteLine($"Max = {b}");
+}
+else
+{
+    WriteLine($"Max = {c}");
+}
