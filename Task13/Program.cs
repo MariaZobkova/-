e36d680bc1319268f0e624f04bbd523a2f545c04
Maria <mzobkova@bk.ru>
2022-12-20 17:00:16 +0300

    Task 13 added

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(ReadLine()!);
+
+int a = (number/100);
+int b = (a%10);
+
+
+if (number <= 99)
+{
+    WriteLine("Третьей цифры числа нет");
+}
+else
+{
+    WriteLine(b);
+}
+
