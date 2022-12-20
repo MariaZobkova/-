e3e2886d4c5d6a6549d79396e45c8b02b39419f0
Maria <mzobkova@bk.ru>
2022-12-20 10:53:00 +0300

    Task 10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+
+int a1 = number/100;
+int a2 = number/10;
+int a3 = number%10;
+int a4 = a2%10;
+
+
+Write(a4);
+
