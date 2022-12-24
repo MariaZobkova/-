6c474893322a32fc70de3057acbe7982fd13d585
Maria <mzobkova@bk.ru>
2022-12-24 21:11:09 +0300

    Task 23 added

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Write("Введите число:");
+bool isParse = int.TryParse(ReadLine(), out int i);
+if (!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+if (i < 1)
+{
+    WriteLine("Число должно быть натуральным");
+}
+int n = 1;
+
+while (n <= i)
+{
+    Write(Math.Pow(n, 3) + "  ");
+    n++;
+}
