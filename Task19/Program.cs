6c44468eb17b87381b48de8407fc26264b4615fe
Maria <mzobkova@bk.ru>
2022-12-24 15:10:15 +0300

    Task 19 added

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+WriteLine("Введите пятизначное число: ");
+
+bool isParse = int.TryParse(ReadLine(), out int number);
+if (!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+
+int a = (number/10000);
+int b = (number/1000%10);
+int c = (number/10%10);
+int d = (number%10);
+
+
+if (a == d && b == c)
+{
+    WriteLine("Полиндром");
+}
+else
+{
+    WriteLine("Не полиндром");
+}
