6bbef67fe63e89fa5998fe25f9ae277bb946f2d7
Maria <mzobkova@bk.ru>
2023-01-13 12:28:22 +0300

    Task 25 added

---
+++ Task25/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// В задаче не использвать стандартный метод Pow!
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+using static System.Console;
+
+WriteLine ("Введите число ");
+int A = int.Parse(ReadLine()!);
+
+WriteLine ("Введите степень числа ");
+int B = int.Parse(ReadLine()!);
+
+
+int expAB = Exp(A, B);
+WriteLine($"Число в указаной степени равно {expAB} ");
+
+
+
+int Exp (int i, int j)
+{
+    int result = 1;
+
+    if (j < 0) return 0;
+
+    
+    for (int g = 0; g < j; g++)
+    {
+  
+       result *= i;
+        
+    }
+
+    return result;
+}
+
