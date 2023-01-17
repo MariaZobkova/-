b4a25107ae0a01cb020caacedb449b7d828d9357
Maria <mzobkova@bk.ru>
2023-01-17 15:16:00 +0300

    Task 34 added

---
+++ task34/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 34: Напишите программу реализующую методы формирования массива, заполненного случайными положительными трёхзначными числами, и подсчета количества чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+
+WriteLine("Введите размер массива" );
+
+bool isParse = int.TryParse(ReadLine(), out int size);
+if (!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+
+int []arr = GetAndPrintArray (size);
+
+WriteLine();
+
+int evenNumber = EvenValue(arr);
+WriteLine($"Количество четных элементов в массиве равно {evenNumber}");
+
+
+int[] GetAndPrintArray (int size)
+{
+    int [] newArray = new int [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(100, 1000);
+        Write($"{newArray[i]} ");
+    }
+
+    return newArray;
+}
+
+int EvenValue (int [] array)
+{
+    int result = 0;
+
+    foreach (var a in array)
+    {
+        if (a % 2 == 0) result++;
+    }
+
+    return result;
+
+}
