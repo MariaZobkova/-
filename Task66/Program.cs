eca90e0f9158660312c00ffe16b7d6d3b76c3be2
Maria <mzobkova@bk.ru>
2023-01-31 22:15:49 +0300

    Task 66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static System.Console;
+
+
+
+WriteLine ("Введите два числа через пробел");
+int[] Parameters = Array.ConvertAll(ReadLine().Split(new string[] { " ", ", ", ";" }, StringSplitOptions.RemoveEmptyEntries), Convert.ToInt32);
+
+
+WriteLine(GetNumbers(Parameters[0], Parameters[1]));
+
+
+int GetNumbers(int numOne, int numTwo)
+{
+    if (numOne == numTwo) return numOne;
+
+    else if (numOne < numTwo)
+    {
+        return (numOne + GetNumbers(numOne + 1, numTwo));
+    }
+    else 
+    {
+        return (numOne + GetNumbers(numOne - 1, numTwo));
+    }
+}
