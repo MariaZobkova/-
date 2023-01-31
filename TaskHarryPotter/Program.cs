888c00dff08069d03625ad93d230a18df0bfc651
Maria <mzobkova@bk.ru>
2023-01-31 22:21:11 +0300

    Task about Harry P added

---
+++ TaskHarryPotter/Program.cs
@@ -0,0 +1,32 @@
+﻿// Tom Marvolo Riddle => I am Lord Voldemort
+// Можно ли с одной строки собрать вторую?
+
+
+using static System.Console;
+using static System.Array;
+Clear();
+
+Write("Введите строку ");
+string str1 = ReadLine()!;
+
+Write("Введите вторую строку ");
+string str2 = ReadLine()!;
+
+
+ChecMagic(str1, str2);
+
+void ChecMagic (string One, string Two)
+{
+    One = One.ToLower();
+    Two = Two.ToLower();
+
+    char[] charOne = One.ToCharArray();
+    char[] charTwo = Two.ToCharArray();
+
+    Array.Sort(charOne);
+    Array.Sort(charTwo);
+
+    bool result = Enumerable.SequenceEqual(charOne, charTwo);
+    
+    WriteLine(result);
+}
